3e2fc32e1b4fd79b443eaabb31ee554ff035d038
leolm83 <leonardo.mateus01@hotmail.com>
2024-11-15 16:33:31 -0300

    exibindo APP_NAME no log

--- src/main/java/com/leolmcoding/simple_server/controllers/HomeController.java
+++ src/main/java/com/leolmcoding/simple_server/controllers/HomeController.java
@@ -1,12 +1,21 @@
 package com.leolmcoding.simple_server.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Value("${APP_NAME}") // desta forma lanca excecao se nao houver a variaviavel de ambiente e nem um "valor padrao"
+    private String appName;
+
+//    @Value("#{environment.APP_NAME}")  // desta forma retorna null caso nao seja setado
+//    private String appName1;
+
     @GetMapping("/") 
     public String home() {
+        System.out.println(appName);
         return "static/templates/index.html";
     }
 }
