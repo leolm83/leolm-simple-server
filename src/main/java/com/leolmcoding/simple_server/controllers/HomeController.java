3802056d1118af99edfe4df2bd0da4851d7301a2
leolm83 <leonardo.mateus01@hotmail.com>
2024-11-16 00:44:20 -0300

    loadbalancing and logging

--- src/main/java/com/leolmcoding/simple_server/controllers/HomeController.java
+++ src/main/java/com/leolmcoding/simple_server/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package com.leolmcoding.simple_server.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
     @Value("${APP_NAME}") // desta forma lanca excecao se nao houver a variaviavel de ambiente e nem um "valor padrao"
     private String appName;
 
@@ -15,7 +17,7 @@ public class HomeController {
 
     @GetMapping("/") 
     public String home() {
-        System.out.println(appName);
+        LOGGER.info("REQUISICAO ENVIADA PARA : {}",appName);
         return "static/templates/index.html";
     }
 }
